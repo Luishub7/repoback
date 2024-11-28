2cb64792c69122c1c8548078b7b3543f70c03c3c
luis.dev <luisro43@gmail.com>
2024-11-28 18:45:54 -0300

    ..

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,3 @@
-// src/routes/authRoutes.js
 import express from 'express';
 import { register, login, verifyEmail, getMe } from '../controllers/authController.js';
 import { verifyToken } from '../middlewares/authMiddleware.js';
